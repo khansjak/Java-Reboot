c00836a014f0cf6d2b565cffca1f0cedbb1dec47
JAK <khans.jak@gmail.com>
2020-04-25 10:58:20 +0530

    Abstract Classes

---
+++ workspace/src/HelloMain.java
@@ -0,0 +1,19 @@
+import models.Employee;
+import models.Person;
+import models.User;
+
+public class HelloMain {
+    public static void main(String[] args) {
+        Person person = new Person();
+        Employee employee = new Employee();
+
+        person.displayDetails();
+        employee.displayDetails();
+
+        User user = new User();
+        user.setUserId(222);
+        user.setUserName("King Drake");
+        System.out.println(user.getUserId());
+        System.out.println(user.getUserName());
+    }
+}
