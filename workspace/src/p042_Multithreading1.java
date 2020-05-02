5d2e3d9431b0ec81756c743ef9bf42078b2b1803
JAK <khans.jak@gmail.com>
2020-05-02 12:45:22 +0530

    Multithreading

---
+++ workspace/src/p042_Multithreading1.java
@@ -0,0 +1,15 @@
+class firstThread extends Thread{
+    public void run(){
+        System.out.println("First Thread running ...");
+    }
+}
+
+public class p042_Multithreading1 {
+    public static void main(String[] args) {
+        firstThread obj = new firstThread();
+        obj.start();
+        //obj.start();
+    }
+}
+
+
